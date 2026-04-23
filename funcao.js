5ff18a0fc85d7ea6642cd0ba8e9fd7729a3c8494
guilhermemarcos661-debug <guilhermemarcos661@gmail.com>
2026-04-23 16:59:19 -0400

    Add files via upload

---
+++ funcao.js
@@ -0,0 +1,37 @@
+/*
+//EXOO1
+function saudacao(){
+    let nome = "guilherme"
+    console.log("olá ",nome)
+    
+}
+saudacao()
+
+//EX002
+function soma (a,b){
+    return a + b
+}
+console.log(soma(6,5))
+
+//EX003
+function maior(a,b){
+    if (a > b) {
+        return a
+    }else{
+        return b
+}
+}
+console.log(maior(10,46))
+
+*/
+//EX004
+function ParOuImpar(numero){
+    if(numero % 2 == 0 ){
+        return "par"
+    }else{
+        return "impar"
+    }
+    
+}
+console.log(ParOuImpar())
+console.log(ParOuImpar())
